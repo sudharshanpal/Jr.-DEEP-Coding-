4577286e05c8af89f4d197791878cf9a5b779a61
sudharshanpal <sudharshanpalaniyappan12@gmail.com>
2025-06-24 14:46:36 -0400

    created starter code file

--- 5_6_coding/tuesday/number_guesser.py
+++ 5_6_coding/tuesday/number_guesser.py
@@ -10,15 +10,15 @@ else:
 # If the user's guess is correct, it congratulates them; otherwise, it reveals the correct number.
 
 # An alternate version is to use a while loop to allow multiple guesses:
-randomNum = random.randint(1, 10)
-while True:
-    inputNum = int(input("Guess a number between 1 and 10: "))
-    if inputNum == randomNum:
-        print("Congratulations! You guessed the right number.")
-        break
-    elif inputNum < randomNum:
-        print("The number is higher. Try again.")
-    elif inputNum > randomNum:
-        print("The number is lower. Try again.")
+# randomNum = random.randint(1, 10)
+# while True:
+#     inputNum = int(input("Guess a number between 1 and 10: "))
+#     if inputNum == randomNum:
+#         print("Congratulations! You guessed the right number.")
+#         break
+#     elif inputNum < randomNum:
+#         print("The number is higher. Try again.")
+#     elif inputNum > randomNum:
+#         print("The number is lower. Try again.")
 # This version continues to prompt the user until they guess the correct number.
 # This code generates a random number between 1 and 10 and allows the user to guess it multiple times.
